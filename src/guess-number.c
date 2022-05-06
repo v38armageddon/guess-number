266b7398c3b0a8f9b544a072205eca8e073c69e4
v38armageddon <thev38armageddon@outlook.fr>
2022-05-06 12:56:56 +0200

    Update guess-number.c and adding Makefile

--- src/guess-number.c
+++ src/guess-number.c
@@ -1,32 +1,29 @@
 #include <stdio.h>
 
-// Init randomization
-long randNumber;
+/* Init randomization */
+int randNumber;
 int answer;
 int guess;
 
-int main(){
+int main() {
     randNumber = rand(13);
-    while (guess!=0){
-        // Ask the user to guess
-        printf("Guess a number between 1 and 13:");
-        scanf("%d", &answer);
+    /* Ask the user to guess */
+    printf("Guess a number between 1 and 13: \n");
+    scanf("%d", &answer);
     
-        // Check if it's the correct number
-        if(answer == randNumber);{
-            printf("You guessed it!");
-            printf("The number was %d", randNumber);
-        }
-
-        if(answer > randNumber);{
-            printf("Sorry, the number you guessed was too high.");
-            printf("The number was %d", randNumber);
-        }
-
-        if(answer < randNumber);{
-            printf("Sorry, the number you guessed was too low.");
-            printf("The number was %d", randNumber);
-        }
+    /* Check if it's the correct number */
+    if(answer == randNumber);{
+        printf("You guessed it!");
+        printf("The number was %d", randNumber);
     }
-    return 0;
+
+    if(answer > randNumber);{
+        printf("Sorry, the number you guessed was too high.");
+        printf("The number was %d", randNumber);
+     }
+
+    if(answer < randNumber);{
+        printf("Sorry, the number you guessed was too low.");
+        printf("The number was %d", randNumber);
+     }
 }
