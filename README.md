7de779236c6495734de3965273cac8e59c726f30
v38armageddon <thev38armageddon@outlook.fr>
2022-05-07 18:39:36 +0200

    Guess number v1.0

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # Guess Number
 
-This is a stupid CLI game for guessing a game in C.
+This is a stupid CLI game in C.
 
 You need to guess a number between 1 and 13.
-
-# Working in progress!
--- src/guess-number.c
+++ src/guess-number.c
@@ -3,27 +3,32 @@
 /* Init randomization */
 int randNumber;
 int answer;
-int guess;
+int count = 1;
+int upper = 13;
+int lower = 1;
 
 int main() {
-    randNumber = rand(13);
+    srand(time(0));
+    for (int i = 0; i < count; i++){
+        randNumber = (rand() % (upper - lower + 1)) + lower;
+    };
     /* Ask the user to guess */
     printf("Guess a number between 1 and 13: \n");
     scanf("%d", &answer);
     
     /* Check if it's the correct number */
-    if(answer == randNumber);{
+    if(answer==randNumber){
         printf("You guessed it!");
-        printf("The number was %d", randNumber);
+        printf("The number was: %d.", randNumber);
     }
 
-    if(answer > randNumber);{
+    else if(answer>randNumber){
         printf("Sorry, the number you guessed was too high.");
-        printf("The number was %d", randNumber);
+        printf("The number was: %d.", randNumber);
      }
 
-    if(answer < randNumber);{
+    else if(answer<randNumber){
         printf("Sorry, the number you guessed was too low.");
-        printf("The number was %d", randNumber);
+        printf("The number was: %d.", randNumber);
      }
 }
